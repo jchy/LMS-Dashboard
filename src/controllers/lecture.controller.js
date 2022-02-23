a6f6cc5388162ca9a1f3bb542d3e81e0dd7c8883
jchy <recjaswantcse@gmail.com>
2022-02-23 21:53:58 +0530

    lecture delte and patch has been implemented

--- src/controllers/lecture.controller.js
+++ src/controllers/lecture.controller.js
@@ -17,6 +17,42 @@ router.post('/lectures', async (req, res) => {
 router.get('/lectures', protect, async (req, res) => {
     const lecture = await Lecture.find({}).populate("author_id").lean().exec();
     return res.status(200).json({ data : lecture });
-})
+});
+
+router.delete('/lectures/:lecture_id', async (req,res)=>{
+    try{
+        const lecture = await Lecture.findOneAndDelete({ _id: req.params.lecture_id })
+        if(!lecture) return res.status(404).json({msg: "Lecture not found"})
+        res.status(200).json(lecture)
+    }
+    catch(err){
+        return res.status(400).json({msg: "Something went wrong!"})
+    }
+});
+
+
+router.patch('/lectures/:lecture_id' ,async (req,res)=>{
+    try{
+        if(!req.body.title) return res.status(400).json({msg: "Name is required"});
+        const lecture = await Lecture.findOneAndUpdate({ 
+            _id: req.params.lecture_id
+        },{
+            $set: {
+                title: req.body.title,
+                batch: req.body.batch
+            }
+        },{
+            returnOriginal: false
+        }
+        )
+        if(!lecture) return res.status(404).json({msg: "Lecture not found"})
+        res.status(200).json(lecture)
+    }
+    catch(err){
+        return res.status(400).json({msg: "Something went wrong!"})
+    }
+});
+
+
 
 module.exports = router;
