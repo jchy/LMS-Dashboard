04a9dcd05c9ce3311c57243b07b155303f25360d
jchy <recjaswantcse@gmail.com>
2022-02-23 20:58:52 +0530

    routes for the lectures,students, users has been created

--- server.js
+++ server.js
@@ -8,11 +8,18 @@ app.use(express.json());
 
 const {signup , signin} = require('./src/controllers/auth.controller');
 const userController = require('./src/controllers/user.controller');
+const lectureController = require('./src/controllers/lecture.controller');
+const studentController = require('./src/controllers/student.controller');
+
 
 app.post("/signup", signup);
 app.post("/signin", signin);
 
 app.use("/users", userController);
+app.use("/lectures", lectureController);
+app.use("/students", studentController);
+
+
 
 const start = async () => {
     await connect();
