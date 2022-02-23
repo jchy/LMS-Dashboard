8e93f2efee4867bee4a0883f7567973e01147bdd
jchy <recjaswantcse@gmail.com>
2022-02-23 20:33:33 +0530

    created MVC architecture and protection and user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,43 @@
+const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    profile_photo_url: {type: String, required: true},
+    roles : {type: String, required: true}
+},{
+    timestamps: { created_at: () => Date.now() }
+});
+
+// pre save hook is provided by mongoose this says that before you save the document 
+// Please do this then save the document
+
+userSchema.pre("save", function(next){
+    if(!this.isModified("password")) return next();
+ 
+    bcrypt.hash(this.password, 8, (err, hash) => {
+        if(err) return next(err);
+
+        this.password = hash;
+        next();
+    })
+
+})
+
+userSchema.methods.checkPassword = function(password){
+    const passwordHash = this.password;
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, passwordHash, (err, same)=>{
+            // console.log("err, same", err, same);
+            if(err) return reject(err);
+
+            resolve(same);
+        });
+    });
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
